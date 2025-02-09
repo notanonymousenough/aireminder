d63e75697741d31682ed20335f9bda2fa173bae6
Denis Mulin <notanonenough@yandex-team.ru>
2025-02-09 17:33:28 +0300

    Fix server timezone

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@ set -e
 
 sudo apt update
 sudo apt upgrade -y
+sudo timedatectl set-timezone Europe/Moscow
 sudo apt install -y git python3.12 python3.12-full python3.12-dev python3.12-venv
 sudo rm -frd /opt/aireminder
 sudo mkdir -p /opt/aireminder
